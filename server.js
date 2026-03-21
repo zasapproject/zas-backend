ab8f43c9f7c32b2e658e163e698befdc37d8114e
jjrch <tu@email.com>
2026-03-21 13:06:15 -0500

    fix: aumentar limite payload 50mb

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ const suscripcionesRouter = require('./routes/suscripciones');
 // Iniciar servidor
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static('public'));
+
 
 
 
